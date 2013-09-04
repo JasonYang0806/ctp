05bc819bf78fbc804ce7c53498960eccb5e1e0e0
jason yang <jasonyl0806@gmail.com>
2013-09-03 23:37:48 -0400

    Commit Leetcode: Multiply Strings (Added clearer code)

--- src/java/leetcode/MultiplyStrings.java
+++ src/java/leetcode/MultiplyStrings.java
@@ -41,3 +41,38 @@ public class Solution {
         }
     }
 }
+
+public class Solution{
+    public String multiply(String num1, String num2){
+        if(num1 == null || num1.equals("0") || num2 == null || num2.equals("0")) return "0";
+
+        int len1 = num1.length(), len2 = num2.length();
+        int[] num = new int[len1 + len2];
+
+        for(int i = len1 - 1; i >= 0; i--){
+            for(int j = len2 - 1; j >= 0; j--){
+                num[i + j + 1] += (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+            }
+        }
+
+        int carry = 0;
+        for(int i = len1 + len2 - 1; i >= 0; i--){
+            num[i] += carry;
+            carry = num[i] / 10;
+            num[i] %= 10;
+        }
+
+        int start;
+        if(num[0] == 0){
+            start = 1;
+        }else{
+            start = 0;
+        }
+
+        StringBuilder res = new StringBuilder();
+        for(int i = start; i <= len1 + len2 - 1; i++){
+            res.append(num[i]);
+        }
+        return res.toString();
+    }
+}
