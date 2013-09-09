42fa01a6a7eadcc1ef06cca2ccce66d087b4346f
jason yang <jasonyl0806@gmail.com>
2013-09-09 13:36:08 -0400

    Update Leetcode: Pascal Triangle (Do not locate to 'child' a space every iteration)

--- src/java/leetcode/PascalTriangle.java
+++ src/java/leetcode/PascalTriangle.java
@@ -6,18 +6,17 @@ public class Solution {
         
         if(numRows <= 0) return res;
         
-        int[] parent, child;
+        int[] parent = new int[numRows];
+        int[] child = new int[numRows];
         ArrayList<Integer> tmpList = new ArrayList<Integer>();
         
         tmpList.add(1);
         res.add((ArrayList<Integer>)tmpList.clone());
         tmpList.clear();
-        parent = new int[1];
         parent[0] = 1;
         
         if(numRows >= 2){
             for(int i = 2; i <= numRows; i++){
-                child = new int[i];
                 child[0] = 1;
                 child[i - 1] = 1;
                 for(int j = 1; j < i - 1; j++){
@@ -26,7 +25,7 @@ public class Solution {
                 for(int k = 0; k < i; k++) tmpList.add(child[k]);
                 res.add((ArrayList<Integer>)tmpList.clone());
                 tmpList.clear();
-                parent = child;
+                for(int s = 0; s < i; s++) parent[s] = child[s];
             }
         }
         return res;
