ba058256979ac48ea9e20e16b7274fd6e0a43ef1
jason yang <jasonyl0806@gmail.com>
2013-09-12 00:51:33 -0400

    Commit Leetcode: Remove Depulicates from Sorted List

---
+++ src/java/leetcode/RemoveDuplicatesFromSortedList_II.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if(head == null) return null;
+        // For head duplicates
+        ListNode headPtr = head, headNextPtr = head.next;
+        while(headPtr != null && headNextPtr != null && headPtr.val == headNextPtr.val){
+            while(headNextPtr != null && headPtr.val == headNextPtr.val){
+                headNextPtr = headNextPtr.next;
+            }
+            if(headNextPtr == null) return null;
+            else{
+                headPtr = headNextPtr;
+                headNextPtr = headPtr.next;
+            }
+        }
+        // First if is redundancy?
+        if(headPtr == null){
+            return null;
+        }else if(headNextPtr == null){
+            return headPtr;
+        }
+        
+        head = headPtr;
+        ListNode prePtr = head, ptr = head.next, nextPtr = head.next.next;
+        while(ptr != null && ptr.next != null){
+            while(ptr.next != null && ptr.val != ptr.next.val){
+                prePtr = ptr;
+                ptr = ptr.next;
+            }
+            if(ptr.next == null) return head;
+            else{
+                nextPtr = ptr.next;
+                while(nextPtr != null && ptr.val == nextPtr.val){
+                    nextPtr = nextPtr.next;
+                }
+                if(nextPtr == null){
+                    prePtr.next = null;
+                    return head;
+                }else{
+                    prePtr.next = nextPtr;
+                    ptr = nextPtr;
+                }
+            }
+        }
+        return head;
+    }
+}
